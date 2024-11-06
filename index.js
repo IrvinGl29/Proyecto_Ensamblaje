e50ab8412ee1cc8ef78f506aca8c39cb80490024
Irvin <irvingarcia.inform3@gmail.com>
2024-11-05 22:13:46 -0600

    Implementacion Cors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors'); // Importa el paquete cors
 require('dotenv').config();
-const { router: authRouter, validateToken } = require('./src/routes/auth'); // Importa el router y el middleware
-const db = require('./config/db');  // Asegúrate de que la ruta sea correcta
+const { router: authRouter, validateToken } = require('./src/routes/auth');
+const db = require('./config/db');
 
 const app = express();
 
@@ -11,15 +12,19 @@ app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
 
 // Middlewares
+app.use(cors()); // Aplica CORS a todas las rutas
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+
 // Rutas de autenticación
 app.use('/api/auth', authRouter);
 
+
 // Aplica el middleware de validación a todas las rutas
-app.use(validateToken); // Protege todas las rutas a partir de aquí
+app.use(validateToken);
 
 // Rutas modularizadas
 app.use('/api/components', require('./src/routes/components'));
@@ -32,7 +37,7 @@ const port = app.get('port');
 app.listen(port, '0.0.0.0', (err) => {
     if (err) {
         console.error(`Error starting server: ${err.message}`);
-        process.exit(1); // Termina el proceso si hay un error
+        process.exit(1);
     } else {
         console.log(`Server running on http://localhost:${port}`);
     }
